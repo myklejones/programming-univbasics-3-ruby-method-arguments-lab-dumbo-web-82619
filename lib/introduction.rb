ea2f16adabad2c83d13908a19ade4faafaeb21fd
Mykle jones <mykle.j91@gmail.com>
2019-08-06 09:53:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,7 @@ puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language_optional(name,language)
-  puts "Hi, my name is #{name} I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
 
